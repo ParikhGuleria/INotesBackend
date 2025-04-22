893bd316f6342c115f4796a58dc8385fc652abf5
ParikhGuleria <parikhguleria801@gmail.com>
2025-04-22 10:48:36 +0530

    path change

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mongoose =require('mongoose');
 
-const mongoURI="mongodb+srv://parikhguleria801:parikh09@inotescluster.shswmql.mongodb.net/INotes?retryWrites=true&w=majority&appName=inotescluster";
+const MONGOURL="mongodb+srv://parikhguleria801:parikh09@inotescluster.shswmql.mongodb.net/INotes?retryWrites=true&w=majority&appName=inotescluster";
 
 const connectToMongo= async()=>{
     try{
-        await mongoose.connect(mongoURI);
+        await mongoose.connect(MONGOURL);
         console.log("Connected to database");
     }
     catch(error){
